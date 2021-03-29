d5f15ba15d4611af9c1236ac1dcfe3111a6ef10e
Mridu-Shukla <mridushukla99@gmail.com>
2021-03-29 23:33:10 +0530

    Sample Test

--- frontend/App.js
+++ frontend/App.js
@@ -6,6 +6,12 @@ import { NavigationContainer } from "@react-navigation/native";
 import FlashMessage from "react-native-flash-message";
 import deviceStorage from './services/deviceStorage.js'; 
 import Screens from './navigation/Screens';
+import * as TaskManager from 'expo-task-manager';
+import * as BackgroundFetch from 'expo-background-fetch';
+
+
+
+
 export default class App extends React.Component {
   constructor(){
     super();
@@ -27,6 +33,14 @@ export default class App extends React.Component {
     });
   }
 
+  componentDidMount(){
+
+    registerFetchTask();
+    // BackgroundFetch.registerTaskAsync('firstTask', {
+    //   minimumInterval: 1
+    // });
+    // console.log(BackgroundFetch.getStatusAsync())
+  }
 
   render() {
     
@@ -40,4 +54,48 @@ export default class App extends React.Component {
           
     );
   }
+}
+
+TaskManager.defineTask('firstTask', () => {
+  console.log("Firsttask")
+});
+
+const FETCH_TASKNAME = 'test_task'
+const INTERVAL = 1
+
+function test() {
+    console.log('function is running')
+}
+
+async function registerFetchTask() {
+    TaskManager.defineTask(FETCH_TASKNAME, test);
+
+    const status = await BackgroundFetch.getStatusAsync();
+    switch (status) {
+        case BackgroundFetch.Status.Restricted:
+        case BackgroundFetch.Status.Denied:
+            console.log("Background execution is disabled");
+            return;
+
+        default: {
+            console.debug("Background execution allowed");
+
+            let tasks = await TaskManager.getRegisteredTasksAsync();
+            if (tasks.find(f => f.taskName === FETCH_TASKNAME) == null) {
+                console.log("Registering task");
+                await BackgroundFetch.registerTaskAsync(FETCH_TASKNAME, {
+                  minimumInterval:INTERVAL
+                });
+
+                tasks = await TaskManager.getRegisteredTasksAsync();
+                console.debug("Registered tasks", tasks);
+            } else {
+                console.log(`Task ${FETCH_TASKNAME} already registered, skipping`);
+            }
+
+            console.log("Setting interval to", INTERVAL);
+            // await BackgroundFetch.setMinimumIntervalAsync(INTERVAL);
+        }
+    }
+
 }
